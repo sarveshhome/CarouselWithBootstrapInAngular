dd2cc4a53b18c2696d073ec170679e3190a861c8
sarveshhome <sarvesh.carlton@gmail.com>
2021-06-18 20:58:52 +0100

    3 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { ImgcarouselComponent } from './imgcarousel/imgcarousel.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, NgbModule],
+  declarations: [AppComponent, HelloComponent, ImgcarouselComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
